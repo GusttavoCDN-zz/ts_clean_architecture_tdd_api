1b8ba02751099fb9fa9a442fb14bc256302485e9
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-10-25 17:31:01 -0300

    test: DbAddAccount class. garatindo que a classe ira chamar corretamente o Encrypter e lançaram exceções corretamente caso o Encrypter lance alguma

---
+++ src/data/useCases/addAccount/DbAddAccount.spec.ts
@@ -0,0 +1,42 @@
+import { IEncrypter } from '../../contracts/IEncrypter';
+import { DbAddAccount } from './DbAddAccount';
+
+interface SutTypes {
+  sut: DbAddAccount
+  encrypterStub: jest.Mocked<IEncrypter>
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub: jest.Mocked<IEncrypter> = {
+    encrypt: jest.fn().mockResolvedValue('hashed_password')
+  };
+
+  const sut = new DbAddAccount(encrypterStub);
+
+  return { sut, encrypterStub };
+}
+
+describe('DbAddAccount UseCase', () => {
+  it('Should call Encrypter with correct password', async () => {
+    const { sut, encrypterStub } = makeSut();
+
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
+
+    const account = { name: 'valid_name', email: 'valid_email', password: 'valid_password' };
+
+    await sut.add(account);
+    expect(encryptSpy).toHaveBeenCalledWith(account.password);
+  });
+
+  it('Should throw a exception if Encrypt throws', async () => {
+    const { encrypterStub, sut } = makeSut();
+
+    encrypterStub.encrypt.mockRejectedValueOnce(new Error());
+
+    const account = { name: 'valid_name', email: 'valid_email', password: 'valid_password' };
+
+    const promise = sut.add(account);
+
+    await expect(promise).rejects.toThrow();
+  });
+});
