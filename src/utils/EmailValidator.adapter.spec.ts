78026b5f34083712a7b3cac158f645a893aba3b5
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-10-25 15:45:40 -0300

    test: EmailValidatorAdapter. Garantindo que retornada true ou false caso email seja valido ou não

--- src/utils/EmailValidator.adapter.spec.ts
+++ src/utils/EmailValidator.adapter.spec.ts
@@ -1,9 +1,33 @@
 import { EmailValidatorAdapter } from './EmailValidator.adapter';
+import Validator from 'validator'
+
+beforeAll(() => {
+  jest.spyOn(Validator, 'isEmail').mockReturnValue(true);
+})
 
 describe('EmailValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter();
+
+    jest.spyOn(Validator, 'isEmail').mockReturnValueOnce(false);
+
     const isValid = sut.isValid('invalid_email@gmail.com');
     expect(isValid).toBe(false);
+  });
+
+  it('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid('valid_email@gmail.com');
+
+    expect(isValid).toBe(true);
+  });
+
+  it('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter();
+
+    const validatorSpy = jest.spyOn(Validator, 'isEmail');
+
+    sut.isValid('any_email@gmail.com');
+    expect(validatorSpy).toHaveBeenCalledWith('any_email@gmail.com');
   });
 });
