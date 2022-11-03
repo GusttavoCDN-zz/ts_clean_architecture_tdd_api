04d0cfaf0409dae025c18d6869918f7df52fabd3
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-11-03 16:19:32 -0300

    refactor: add conexão com mongoDB para testes de integração na rota signup

--- src/main/routes/signup.routes.test.ts
+++ src/main/routes/signup.routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest';
+import { MongoHelper } from '../../infra/databases/mongodb/helpers/mongo.helper';
 import app from '../config/app';
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
   it('Should return an account on sucess', async () => {
     await request(app)
       .post('/api/signup')
