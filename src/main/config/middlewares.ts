622bb320a7285f067b65eeb33a82d65ecf4bda50
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-11-03 14:41:19 -0300

    feat: garantido que cors esteja habilitado.

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Application } from 'express';
 import { bodyParser } from '../middlewares/body-parser';
+import { corsMiddleware } from '../middlewares/cors';
 
 export default (app: Application): void => {
   app.use(bodyParser);
+  app.use(corsMiddleware);
 };
