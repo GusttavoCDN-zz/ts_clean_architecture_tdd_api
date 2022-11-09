d703a7a84dbd8a3b18b10c2c8d1d8fcfaf7f088a
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-11-09 19:50:22 -0300

    test: garantido que log de error sera adicionado no db

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,9 +1,12 @@
+import { ILogErrorRepository } from '../../data/contracts/ILogErrorRepository';
 import { IController } from '../../presentation/contracts';
+import { serverError } from '../../presentation/helpers/http.helpers';
 import { LogControllerDecorator } from './log';
 
 interface SutTypes {
   sut: LogControllerDecorator
   controllerStub: jest.Mocked<IController>
+  logRepositoryStub: jest.Mocked<ILogErrorRepository>
 }
 
 const makeSut = (): SutTypes => {
@@ -19,9 +22,13 @@ const makeSut = (): SutTypes => {
     })
   };
 
-  const sut = new LogControllerDecorator(controllerStub);
+  const logRepositoryStub: jest.Mocked<ILogErrorRepository> = {
+    log: jest.fn().mockResolvedValue(null)
+  };
 
-  return { sut, controllerStub };
+  const sut = new LogControllerDecorator(controllerStub, logRepositoryStub);
+
+  return { sut, controllerStub, logRepositoryStub };
 };
 
 describe('LogController Decorator', () => {
@@ -65,5 +72,27 @@ describe('LogController Decorator', () => {
         password: 'hashed_password'
       }
     });
+  });
+
+  it('Should call LogRepositoryError with correct error if controller returna a server error', async () => {
+    const { sut, controllerStub, logRepositoryStub } = makeSut();
+    const error = new Error();
+    error.stack = 'any_stack';
+
+    const logSpy = jest.spyOn(logRepositoryStub, 'log');
+
+    jest.spyOn(controllerStub, 'handle').mockResolvedValueOnce(serverError(error));
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+
+    await sut.handle(httpRequest);
+    expect(logSpy).toHaveBeenCalledWith('any_stack');
   });
 });
--- src/main/factories/signup.factory.ts
+++ src/main/factories/signup.factory.ts
@@ -1,5 +1,6 @@
 import { DbAddAccount } from '../../data/useCases/addAccount/DbAddAccount';
 import { BcryptAdapter } from '../../infra/bcrypt/BcryptAdapter';
+import { MongoLogRepository } from '../../infra/databases/log-repository/MongoLogRepository';
 import { MongoAccountRepository } from '../../infra/databases/mongodb/MongoAccountRepository';
 import { IController } from '../../presentation/contracts';
 import { SignUpController } from '../../presentation/controllers/signup/SignUp';
@@ -17,5 +18,7 @@ export const makeSignUpController = (): IController => {
   const addAccount = new DbAddAccount(encrypter, mongoAccountRepository);
 
   const signUpController = new SignUpController(emailValidator, addAccount);
-  return new LogControllerDecorator(signUpController);
+  const logErrorRepository = new MongoLogRepository();
+
+  return new LogControllerDecorator(signUpController, logErrorRepository);
 };
